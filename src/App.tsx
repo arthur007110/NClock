e9dce2911632e5f83e8550a5d72da2d58c84ddcc
arthur007110 <arthurcosta007110@gmail.com>
2022-10-23 03:20:38 -0300

    adicionando tasksBar no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { GlobalStyle } from './styles/GlobalStyle'
-
-import { Greetings } from './components/Greetings'
+// import { Greetings } from './components/Greetings'
+import { TasksBar } from './components/TasksBar'
 
 export function App() {
   return (
     <>
+      <TasksBar />
       <GlobalStyle />
-      <Greetings />
+      {/* <Greetings /> */}
     </>
   )
 }
