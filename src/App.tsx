da0853b5316e0fcf9bc22ced76fb76985a52fc3b
arthur007110 <arthurcosta007110@gmail.com>
2022-10-23 23:06:47 -0300

    adicionando elementos no app principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { GlobalStyle } from './styles/GlobalStyle'
 // import { Greetings } from './components/Greetings'
 import { TasksBar } from './components/TasksBar'
+import Inspector from './components/Inspector'
+import { MainApp, MainView } from './styles/appStyles'
+import Clock from './components/Clock'
 
 export function App() {
   return (
-    <>
+    <MainApp>
       <TasksBar />
+      <MainView>
+        <Clock />
+        <Inspector />
+      </MainView>
       <GlobalStyle />
       {/* <Greetings /> */}
-    </>
+    </MainApp>
   )
 }
---
+++ src/styles/appStyles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const MainApp = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`
+
+export const MainView = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
